8821c69de6527dc966d67bd985ded85a8e6de0ef
johnhypnosisbr <john.kmick@yahoo.com.br>
2023-07-23 15:16:50 -0300

    Added products and view

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { sampleProducts } from "./data";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div>
-      <header>
-        TS Amazona
-      </header>
+      <header>TS Amazona</header>
       <main>
-
+        <ul>
+          {sampleProducts.map((product) => (
+            <li key={product.slug}>
+              <img className="product-img" src={product.image} />
+              <h2>{product.name}</h2>
+              <p>US$ {product.price}</p>
+            </li>
+          ))}
+        </ul>
       </main>
-      <footer>
-        All rights reserved
-      </footer>
+      <footer>All rights reserved</footer>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/data.ts
@@ -0,0 +1,52 @@
+import { Product } from "./types/Product";
+
+export const sampleProducts:Product[] = [
+    {
+        name: "Nike Slim Shirt",
+        slug: "nike-slim-shirt",
+        category: 'Shirts',
+        image: "../img/shirt/nike-slim-shirt-fr.png",
+        brand: 'Nike',
+        price: 120,
+        countInStock: 10,
+        description: 'High quality sports shirt',
+        rating: 4.5,
+        numReview: 10
+    },
+    {
+        name: "Adidas Fit Shirt",
+        slug: "adidas-fit-shirt",
+        category: 'Shirts',
+        image: "../img/shirt/adidas-fit-shirt-fr.png",
+        brand: 'Adidas',
+        price: 120,
+        countInStock: 10,
+        description: 'High quality sports shirt',
+        rating: 4.5,
+        numReview: 10
+    },
+    {
+        name: "Adidas Rio Track Pants",
+        slug: "adidas-rio-pants",
+        category: 'Pants',
+        image: "../img/pants/adidas-rio-pants-fr.png",
+        brand: 'Adidas',
+        price: 160,
+        countInStock: 10,
+        description: 'High quality sports pants',
+        rating: 4.1,
+        numReview: 12
+    },
+    {
+        name: "Jeans Slim",
+        slug: "jeans-slim",
+        category: 'Pants',
+        image: "../img/pants/jeans-slim-fr.png",
+        brand: 'Levi',
+        price: 160,
+        countInStock: 10,
+        description: 'High quality jeans',
+        rating: 4.4,
+        numReview: 11
+    }
+]
